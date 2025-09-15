f6e338359900a8ec7b878b925c42057b3a1a499b
johndave100 <59090724+johndave100@users.noreply.github.com>
2025-09-15 05:03:32 +0200

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,3 +1,20 @@
 @echo off
-REM Placeholder gradlew.bat
-java -jar gradle\wrapper\gradle-wrapper.jar %*
+SETLOCAL
+
+set DIRNAME=%~dp0
+if "%DIRNAME%"=="" set DIRNAME=.
+
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+set GRADLE_HOME=%APP_HOME%\gradle\wrapper
+set GRADLE_WRAPPER_JAR=%GRADLE_HOME%\gradle-wrapper.jar
+set GRADLE_WRAPPER_PROPERTIES=%GRADLE_HOME%\gradle-wrapper.properties
+
+if exist "%GRADLE_WRAPPER_JAR%" (
+  java -classpath "%GRADLE_WRAPPER_JAR%" org.gradle.wrapper.GradleWrapperMain %*
+) else (
+  echo ERROR: Gradle wrapper JAR not found at "%GRADLE_WRAPPER_JAR%"
+  exit /b 1
+)
+ENDLOCAL
